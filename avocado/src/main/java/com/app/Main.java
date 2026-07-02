baa56cc6ae261cb74d08b77c255772cc94792450
petar <petarsipka90@gmail.com>
2026-07-02 20:54:05 +0200

    chore: add initial backend project structure

---
+++ avocado/src/main/java/com/app/Main.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+public class Main {
+    public static void main(String[] args) {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+
+        KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+        kSession.dispose();
+    }
+}
